f12337e6d0181777840940dbc53d808bdbcbd067
mr-guest <62317545+mr-guest@users.noreply.github.com>
2021-03-23 20:57:40 +0800

    Add files via upload
    I still dont know how github works

---
+++ Vim/vim.vimrc
@@ -0,0 +1,37 @@
+
+set history=500
+filetype plugin on
+filetype indent on 
+set ruler
+set number
+set clipboard=unnamed
+set ignorecase
+set smartcase
+set hlsearch
+set fileformats=unix,dos,mac
+
+
+syntax on
+set encoding=utf8
+colorscheme carbonized-dark
+
+set nobackup
+set nowb
+set noswapfile
+
+call plug#begin('~/.vimfiles/plugged')
+Plug 'scrooloose/nerdtree'
+Plug 'tpope/vim-fugitive'
+Plug 'jceb/vim-orgmode'
+Plug 'nightsense/carbonized'
+Plug 'raimon49/requirements.txt.vim'
+Plug 'neoclide/coc.nvim', {'branch': 'release' , 'for' : ['json','cpp','py','vim','css','html']	}
+Plug 'tmhedberg/SimpylFold'
+augroup vimrc-python
+  autocmd!
+  autocmd FileType python setlocal expandtab shiftwidth=4 tabstop=8 colorcolumn=79
+      \ formatoptions+=croq softtabstop=4
+      \ cinwords=if,elif,else,for,while,try,except,finally,def,class,with
+augroup END
+let python_highlight_all = 1
+call plug#end()
---
+++ emacs.d/init.el
@@ -0,0 +1,134 @@
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                        ("org" . "https://orgmode.org/elpa/")
+						("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)          
+
+;; Dasbhoard
+(use-package dashboard
+  :defer nil
+  :preface
+  :config
+  (dashboard-setup-startup-hook))
+  (setq dashboard-items '((recents . 7)))
+  (setq dashboard-banner-logo-title "Personal Emacs / mr.guest")
+  (setq dashboard-startup-banner "~/.emacs.d/kokoron.png")
+  (setq dashboard-center-content t)
+  (setq dashboard-show-shortcuts t)
+  (setq dashboard-set-init-info t)
+  (setq dashboard-init-info (format "%d packages loaded in %s"
+                                    (length package-activated-list) (emacs-init-time)))
+  (setq dashboard-set-footer nil)
+  (setq dashboard-set-navigator t)
+
+(use-package doom-themes
+  :demand
+  :defer nil
+  :config
+  (load-theme 'doom-one t))
+
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 10)))
+
+;; ivy 
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package counsel)
+
+(use-package which-key
+  :diminish which-key-mode
+  :init
+  (which-key-mode 1))
+
+(use-package minimap
+  :init
+  (minimap-mode 1))
+
+;; Snippets
+(use-package yasnippet
+  :init
+  (yas-global-mode 1))
+
+(use-package yasnippet-snippets)
+
+;; Evil 
+(use-package evil
+  :init
+  (evil-mode 1))
+
+;; Enable copypasting outside of Emacs
+(setq x-select-enable-clipboard t)
+
+;; org-mode 
+(use-package org)
+
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+
+(setq org-todo-keywords
+       '((sequence "TODO"
+		   "PROJ"
+		   "LOOP"
+		   "WAIT"
+		   "|"
+		   "DONE"
+		   "NA" )))
+;; org-roam
+(use-package org-roam
+      :hook
+      (after-init . org-roam-mode)
+      :custom
+      (org-roam-directory "~/Document/org/")
+      :bind (:map org-roam-mode-map
+              (("C-c n l" . org-roam)
+               ("C-c n f" . org-roam-find-file)
+               ("C-c n g" . org-roam-graph))
+              :map org-mode-map
+              (("C-c n i" . org-roam-insert))
+              (("C-c n I" . org-roam-insert-immediate))))
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		eshell-mode-hook))
+  (add-hook mode(lambda ()(display-line-numbers-mode 0))))
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(setq custom-file (concat user-emacs-directory "custom.el"))
+;end of init.el
+
